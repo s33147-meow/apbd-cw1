5c25acb7312e81a3db6c82f9036ef600bf04ebca
s33147 <s33147@pjwstk.edu.pl>
2026-03-11 11:15:28 +0100

    shorten function declaration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-int CalculateAverage(int[] values) {
-	return (int)values.Average();
-}
+int CalculateAverage(int[] values) =>
+	(int)values.Average();
 
 for(int i = 0; i < 44; i++)
 	Console.WriteLine("totally amazing feature! such wow");
