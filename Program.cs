d58653e7834dbe20e4377bde9e95e9dbccbd90af
s33147 <s33147@pjwstk.edu.pl>
2026-03-11 11:17:26 +0100

    intruduce the amazing averaging technology to the masses

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 int CalculateAverage(int[] values) =>
 	(int)values.Average();
 
-for(int i = 0; i < 44; i++)
+for(int i = 0; i < CalculateAverage([10, 44, 67]); i++)
 	Console.WriteLine("totally amazing feature! such wow");
